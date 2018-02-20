05064e8d58f93168141330791b1e41234df5ec3b
Brian Birtles <birtles@gmail.com>
2018-02-20 18:50:30 +0900

    Do some basic kanji entry parsing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 #[macro_use]
 extern crate structopt;
-extern crate xml;
+extern crate quick_xml;
+// extern crate smallvec;
 
-use std::fs::File;
-use std::io::BufReader;
+// TODO: Add this to Cargo.toml (and extern crate)
+// use smallvec::SmallVec;
 use std::path::PathBuf;
 use structopt::StructOpt;
-use xml::reader::{EventReader, ParserConfig, XmlEvent};
+use quick_xml::reader::Reader;
+use quick_xml::events::Event;
 
 #[derive(StructOpt)]
 #[structopt(name = "jmdict-couch")]
@@ -17,43 +19,96 @@ struct Opt {
     input: PathBuf,
 }
 
+/// k_ele from jmdict schema
+#[derive(Debug)]
+struct KanjiEntry {
+    /// keb
+    word: String,
+    /// ke_inf
+    // TODO Use SmallVec below
+    orthography: Vec<String>,
+    /// ke_pri
+    priority: Vec<String>,
+}
+
 fn main() {
     let opt = Opt::from_args();
 
     // TODO: Support reading from stdin?
 
-    let file = File::open(opt.input).expect("Could not read from file");
-    let file = BufReader::new(file);
+    let mut reader = Reader::from_file(opt.input).expect("Could not read from file");
+    reader.trim_text(true);
+    reader.check_end_names(false);
 
-    let mut in_entry = false;
+    let mut buf = Vec::new();
+    let mut kanji_entries = Vec::<KanjiEntry>::new();
 
-    let parser = EventReader::new_with_config(file,
-                                              ParserConfig::new()
-                                              .ignore_comments(false)
-                                              .whitespace_to_characters(true)
-                                              .trim_whitespace(true));
-    for e in parser {
-        match e {
-            Ok(XmlEvent::StartElement { name, .. }) => {
-                if name.local_name == "entry" {
-                    assert!(!in_entry, "Entries should not be nested");
-                    in_entry = true;
-                    println!("{}", name);
+    loop {
+        match reader.read_event(&mut buf) {
+            Ok(Event::Start(ref e)) => {
+                match e.name() {
+                    b"k_ele" => {
+                        kanji_entries.push(parse_k_ele(&mut reader).expect("Failed to parse kanji entry"));
+                    },
+                    _ => (),
                 }
-            }
-            Ok(XmlEvent::EndElement { name }) => {
-                if name.local_name == "entry" {
-                    assert!(in_entry, "Entry tag mismatch");
-                    in_entry = false;
-                }
-            }
-            Ok(XmlEvent::Characters(text)) => {
-                println!("# {}", text);
-            }
-            Err(e) => {
-                println!("Error: {}", e);
-            }
-            _ => {}
+            },
+            Ok(Event::Eof) => break,
+            Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
+            _ => (),
         }
+
+        // If we don't keep a borrow elsewhere, we can clear the buffer to keep memory usage low
+        buf.clear();
     }
+
+    for kanji in kanji_entries {
+        println!("> {:?}", kanji);
+    }
+}
+
+fn parse_k_ele<T: std::io::BufRead>(reader: &mut Reader<T>) -> Result<KanjiEntry, ()> {
+    let mut word: String = String::new();
+    let mut orthography: Vec<String> = Vec::new();
+    let mut priority: Vec<String> = Vec::new();
+
+    enum Elem {
+        Keb,
+        KeInf,
+        KePri,
+    }
+    let mut elem: Option<Elem> = None;
+    let mut buf = Vec::new();
+
+    loop {
+        match reader.read_event(&mut buf) {
+            Ok(Event::Start(ref e)) => {
+                match e.name() {
+                    b"keb" => elem = Some(Elem::Keb),
+                    b"ke_inf" => elem = Some(Elem::KeInf),
+                    b"ke_pri" => elem = Some(Elem::KePri),
+                    _ => (),
+                }
+            },
+            Ok(Event::End(ref e)) => {
+                match e.name() {
+                    b"k_ele" => break,
+                    _ => elem = None,
+                }
+            },
+            Ok(Event::Text(e)) => {
+                match elem {
+                    Some(Elem::Keb) => word = e.unescape_and_decode(&reader).unwrap(),
+                    Some(Elem::KeInf) => orthography.push(e.unescape_and_decode(&reader).unwrap()),
+                    Some(Elem::KePri) => priority.push(e.unescape_and_decode(&reader).unwrap()),
+                    _ => return Err(()),
+                }
+            },
+            Err(_) => return Err(()),
+            _ => (),
+        }
+        buf.clear();
+    }
+
+    Ok(KanjiEntry { word, orthography, priority })
 }
