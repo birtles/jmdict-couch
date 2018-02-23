31411c95f1e1a1850a6ff95f5e09eb2a9b677f49
Brian Birtles <birtles@gmail.com>
2018-02-23 06:03:06 -0500

    Add some error handling

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,25 @@
 #[macro_use]
-extern crate structopt;
+extern crate failure;
 extern crate quick_xml;
 extern crate smallvec;
+#[macro_use]
+extern crate structopt;
 
 // TODO
-// -- The error handling here is embarassing
 // -- The factoring is awkward (really, pass the first tag then get function to pass the rest?)
 //    How is this supposed to work?
 // -- All this could use tests
 // -- Now that this is starting to come together, work out how better to factor out common code.
 //    Macros? Mako?
 
+use failure::{Error, ResultExt};
 use smallvec::SmallVec;
 use std::path::PathBuf;
+use std::str;
 use std::str::FromStr;
 use structopt::StructOpt;
 use quick_xml::reader::Reader;
-use quick_xml::events::Event;
+use quick_xml::events::{BytesText, Event};
 
 #[derive(StructOpt)]
 #[structopt(name = "jmdict-couch")]
@@ -70,7 +73,23 @@ struct ReadingEntry {
 fn main() {
     let opt = Opt::from_args();
 
-    let mut reader = Reader::from_file(opt.input).expect("Could not read from file");
+    let entries = get_entries(&opt.input);
+    if let Err(ref e) = entries {
+        use std::io::Write;
+        let stderr = &mut ::std::io::stderr();
+        writeln!(stderr, "{}", e).expect("Error writing to stderr");
+        ::std::process::exit(1);
+    }
+
+    let entries = entries.unwrap();
+
+    for entry in entries {
+        println!("> {:?}", entry);
+    }
+}
+
+fn get_entries(input: &PathBuf) -> Result<Vec<Entry>, Error> {
+    let mut reader = Reader::from_file(input).context("Could not read from file")?;
     reader.trim_text(true);
     reader.check_end_names(false);
     reader.expand_empty_elements(true);
@@ -80,27 +99,27 @@ fn main() {
 
     loop {
         match reader.read_event(&mut buf) {
-            Ok(Event::Start(ref e)) => {
-                match e.name() {
-                    b"entry" => {
-                        entries.push(parse_entry(&mut reader).expect("Failed to parse entry"));
-                    },
-                    _ => (),
+            Ok(Event::Start(ref e)) => match e.name() {
+                b"entry" => {
+                    entries.push(parse_entry(&mut reader)?);
                 }
+                _ => (),
             },
             Ok(Event::Eof) => break,
-            Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
+            Err(e) => bail!(
+                "Error parsing entry at position #{}: {}",
+                reader.buffer_position(),
+                e
+            ),
             _ => (),
         }
         buf.clear();
     }
 
-    for entry in entries {
-        println!("> {:?}", entry);
-    }
+    Ok(entries)
 }
 
-fn parse_entry<T: std::io::BufRead>(reader: &mut Reader<T>) -> Result<Entry, ()> {
+fn parse_entry<T: std::io::BufRead>(reader: &mut Reader<T>) -> Result<Entry, Error> {
     let mut id: u32 = 0;
     let mut kanji_entries: Vec<KanjiEntry> = Vec::new();
     let mut reading_entries: Vec<ReadingEntry> = Vec::new();
@@ -110,46 +129,67 @@ fn parse_entry<T: std::io::BufRead>(reader: &mut Reader<T>) -> Result<Entry, ()>
 
     loop {
         match reader.read_event(&mut buf) {
-            Ok(Event::Start(ref e)) => {
-                match e.name() {
-                    b"k_ele" => kanji_entries.push(parse_k_ele(reader)?),
-                    b"r_ele" => reading_entries.push(parse_r_ele(reader)?),
-                    b"ent_seq" => {
-                        assert!(!ent_seq);
-                        ent_seq = true;
-                    },
-                    _ => (),
+            Ok(Event::Start(ref e)) => match e.name() {
+                b"k_ele" => kanji_entries.push(parse_k_ele(reader)?),
+                b"r_ele" => reading_entries.push(parse_r_ele(reader)?),
+                b"ent_seq" => {
+                    ensure!(
+                        !ent_seq,
+                        "Nested ent_seq at position #{}",
+                        reader.buffer_position()
+                    );
+                    ent_seq = true;
                 }
+                _ => (),
+                // _ => warn_unknown_tag(e.name(), reader.buffer_position(), "entry"),
             },
-            Ok(Event::End(ref e)) => {
-                match e.name() {
-                    b"entry" => break,
-                    b"ent_seq" => {
-                        assert!(ent_seq);
-                        ent_seq = false;
-                    }
-                    _ => (),
+            Ok(Event::End(ref e)) => match e.name() {
+                b"entry" => break,
+                b"ent_seq" => {
+                    ensure!(
+                        ent_seq,
+                        "Mismatched ent_seq tags at position #{}",
+                        reader.buffer_position()
+                    );
+                    ent_seq = false;
                 }
+                _ => (),
             },
             Ok(Event::Text(e)) => {
                 if ent_seq {
-                    id = u32::from_str(&e.unescape_and_decode(&reader).unwrap()).unwrap();
+                    id = u32::from_str(&e.unescape_and_decode(&reader)?)
+                        .context("Failed to parse ent_seq as int")?;
                 }
-            },
-            Err(_) => return Err(()),
+            }
+            Err(e) => bail!(
+                "Error parsing entry at position #{}: {}",
+                reader.buffer_position(),
+                e
+            ),
             _ => (),
         }
         buf.clear();
     }
 
-    if id == 0 || reading_entries.is_empty() {
-        return Err(())
-    }
+    ensure!(
+        id != 0,
+        "ID not found at position #{}",
+        reader.buffer_position()
+    );
+    ensure!(
+        !reading_entries.is_empty(),
+        "No reading entries found at position #{}",
+        reader.buffer_position()
+    );
 
-    Ok(Entry { id, kanji_entries, reading_entries })
+    Ok(Entry {
+        id,
+        kanji_entries,
+        reading_entries,
+    })
 }
 
-fn parse_k_ele<T: std::io::BufRead>(reader: &mut Reader<T>) -> Result<KanjiEntry, ()> {
+fn parse_k_ele<T: std::io::BufRead>(reader: &mut Reader<T>) -> Result<KanjiEntry, Error> {
     let mut kanji: String = String::new();
     let mut info: InfoVec = InfoVec::new();
     let mut priority: PriorityVec = PriorityVec::new();
@@ -164,40 +204,50 @@ fn parse_k_ele<T: std::io::BufRead>(reader: &mut Reader<T>) -> Result<KanjiEntry
 
     loop {
         match reader.read_event(&mut buf) {
-            Ok(Event::Start(ref e)) => {
-                match e.name() {
-                    b"keb" => elem = Some(Elem::Keb),
-                    b"ke_inf" => elem = Some(Elem::KeInf),
-                    b"ke_pri" => elem = Some(Elem::KePri),
-                    _ => (),
-                }
+            Ok(Event::Start(ref e)) => match e.name() {
+                b"keb" => elem = Some(Elem::Keb),
+                b"ke_inf" => elem = Some(Elem::KeInf),
+                b"ke_pri" => elem = Some(Elem::KePri),
+                _ => warn_unknown_tag(e.name(), reader.buffer_position(), "k_ele"),
             },
-            Ok(Event::End(ref e)) => {
-                match e.name() {
-                    b"k_ele" => break,
-                    _ => elem = None,
-                }
+            Ok(Event::End(ref e)) => match e.name() {
+                b"k_ele" => break,
+                _ => elem = None,
             },
-            Ok(Event::Text(e)) => {
-                match elem {
-                    Some(Elem::Keb) => kanji = e.unescape_and_decode(&reader).unwrap(),
-                    Some(Elem::KeInf) => info.push(e.unescape_and_decode(&reader).unwrap()),
-                    Some(Elem::KePri) => priority.push(e.unescape_and_decode(&reader).unwrap()),
-                    _ => (), // TODO: Make this warn
-                }
+            Ok(Event::Text(e)) => match elem {
+                Some(Elem::Keb) => kanji = e.unescape_and_decode(&reader)?,
+                Some(Elem::KeInf) => info.push(e.unescape_and_decode(&reader)?),
+                Some(Elem::KePri) => priority.push(e.unescape_and_decode(&reader)?),
+                _ => warn_unexpected_text(&e, reader, "k_ele"),
             },
-            Err(_) => return Err(()),
+            Err(e) => bail!(
+                "Error parsing entry at position #{}: {}",
+                reader.buffer_position(),
+                e
+            ),
             _ => (),
         }
         buf.clear();
     }
 
-    // TODO: Check that kanji is not the empty string
+    assert!(
+        kanji.trim() == kanji,
+        "Kanji keys should not have leading or trailing whitespace"
+    );
+    ensure!(
+        !kanji.is_empty(),
+        "Kanji key is empty at position #{}",
+        reader.buffer_position()
+    );
 
-    Ok(KanjiEntry { kanji, info, priority })
+    Ok(KanjiEntry {
+        kanji,
+        info,
+        priority,
+    })
 }
 
-fn parse_r_ele<T: std::io::BufRead>(reader: &mut Reader<T>) -> Result<ReadingEntry, ()> {
+fn parse_r_ele<T: std::io::BufRead>(reader: &mut Reader<T>) -> Result<ReadingEntry, Error> {
     let mut kana = String::new();
     let mut no_kanji = false;
     let mut related_kanji: Vec<String> = Vec::new();
@@ -215,38 +265,79 @@ fn parse_r_ele<T: std::io::BufRead>(reader: &mut Reader<T>) -> Result<ReadingEnt
 
     loop {
         match reader.read_event(&mut buf) {
-            Ok(Event::Start(ref e)) => {
-                match e.name() {
-                    b"reb" => elem = Some(Elem::Reb),
-                    b"re_nokanji" => no_kanji = true,
-                    b"re_restr" => elem = Some(Elem::ReRestr),
-                    b"re_inf" => elem = Some(Elem::ReInf),
-                    b"re_pri" => elem = Some(Elem::RePri),
-                    _ => (), // TODO: This should probably warn
-                }
+            Ok(Event::Start(ref e)) => match e.name() {
+                b"reb" => elem = Some(Elem::Reb),
+                b"re_nokanji" => no_kanji = true,
+                b"re_restr" => elem = Some(Elem::ReRestr),
+                b"re_inf" => elem = Some(Elem::ReInf),
+                b"re_pri" => elem = Some(Elem::RePri),
+                _ => warn_unknown_tag(e.name(), reader.buffer_position(), "r_ele"),
             },
-            Ok(Event::End(ref e)) => {
-                match e.name() {
-                    b"r_ele" => break,
-                    _ => elem = None,
-                }
+            Ok(Event::End(ref e)) => match e.name() {
+                b"r_ele" => break,
+                _ => elem = None,
             },
-            Ok(Event::Text(e)) => {
-                match elem {
-                    Some(Elem::Reb) => kana = e.unescape_and_decode(&reader).unwrap(),
-                    Some(Elem::ReRestr) => related_kanji.push(e.unescape_and_decode(&reader).unwrap()),
-                    Some(Elem::ReInf) => info.push(e.unescape_and_decode(&reader).unwrap()),
-                    Some(Elem::RePri) => priority.push(e.unescape_and_decode(&reader).unwrap()),
-                    _ => (), // TODO: Make this warn
-                }
+            Ok(Event::Text(e)) => match elem {
+                Some(Elem::Reb) => kana = e.unescape_and_decode(&reader).unwrap(),
+                Some(Elem::ReRestr) => related_kanji.push(e.unescape_and_decode(&reader).unwrap()),
+                Some(Elem::ReInf) => info.push(e.unescape_and_decode(&reader).unwrap()),
+                Some(Elem::RePri) => priority.push(e.unescape_and_decode(&reader).unwrap()),
+                _ => warn_unexpected_text(&e, reader, "r_ele"),
             },
-            Err(_) => return Err(()),
+            Err(e) => bail!(
+                "Error parsing entry at position #{}: {}",
+                reader.buffer_position(),
+                e
+            ),
             _ => (),
         }
         buf.clear();
     }
 
-    // TODO: Check that kana is not the empty string
+    assert!(
+        kana.trim() == kana,
+        "Kana keys should not have leading or trailing whitespace"
+    );
+    ensure!(
+        !kana.is_empty(),
+        "Kana key is empty at position #{}",
+        reader.buffer_position()
+    );
 
-    Ok(ReadingEntry { kana, no_kanji, related_kanji, info, priority })
+    Ok(ReadingEntry {
+        kana,
+        no_kanji,
+        related_kanji,
+        info,
+        priority,
+    })
+}
+
+fn warn_unknown_tag(elem_name: &[u8], buffer_position: usize, ancestor: &str) {
+    match str::from_utf8(elem_name) {
+        Ok(tag) => println!(
+            "WARNING: Unrecognized {} member element {} at position #{}",
+            ancestor, tag, buffer_position
+        ),
+        _ => println!(
+            "WARNING: Unrecognized {} member element (non-utf8) at position #{}",
+            ancestor, buffer_position
+        ),
+    }
+}
+
+fn warn_unexpected_text<T: std::io::BufRead>(text: &BytesText, reader: &Reader<T>, ancestor: &str) {
+    match text.unescape_and_decode(reader) {
+        Ok(text) => println!(
+            "WARNING: Unexpected text \"{}\" in {} element at position #{}",
+            text,
+            ancestor,
+            reader.buffer_position(),
+        ),
+        _ => println!(
+            "WARNING: Unexpected text in {} element (non-utf8) at position #{}",
+            ancestor,
+            reader.buffer_position()
+        ),
+    }
 }
